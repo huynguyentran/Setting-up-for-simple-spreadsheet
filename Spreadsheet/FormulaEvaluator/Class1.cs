8be7b95a53a5b3adcdf0cd6fe477de81b34c0c6d
u1315096@eng.utah.edu <u1315096@eng.utah.edu>
2021-01-28 21:01:27 -0700

    0.5 version

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -1,9 +1,206 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FormulaEvaluator
 {
+    public delegate int Lookup(String v);
+
+
     public static class Evaluator
     {
-        // TODO: Follow the PS1 instructions
+
+
+
+        public static int Evaluate(string input, Lookup variable)
+        {
+            string[] substrings = Regex.Split(input, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            Stack<int> valueStack = new Stack<int>();
+            Stack<string> operatorStack = new Stack<string>();
+            foreach (string t in substrings)
+            {
+                //Extension method 
+                if (t.Equals(" "))
+                {
+                    continue;
+                }
+
+                if (int.TryParse(t, out int number))
+                {
+                    if (operatorStack.IsOnTop("*") || operatorStack.IsOnTop("/"))
+                    {
+                        valueStackEmptyCheck(valueStack.Count);
+                        int v = Calculator(number, valueStack.Pop(), operatorStack.Pop());
+                        valueStack.Push(v);
+
+                    }
+                    else
+                    {
+                        valueStack.Push(number);
+                    }
+
+
+                }
+
+                if (VariablesVerification(t))
+                {
+                    if (operatorStack.IsOnTop("*") || operatorStack.IsOnTop("/"))
+                    {
+                        valueStackEmptyCheck(valueStack.Count);
+                        try
+                        {
+                            int v = Calculator(variable(t), valueStack.Pop(), operatorStack.Pop());
+                            valueStack.Push(v);
+                        }
+
+                        catch
+                        {
+                            throw new ArgumentException("Can not find variable.");
+                        }
+
+                    }
+                    else
+                    {
+                        valueStack.Push(variable(t));
+                    }
+                }
+
+           
+      
+
+                if (t.Equals("+") || t.Equals("-"))
+                {
+                    if (operatorStack.IsOnTop("+") || operatorStack.IsOnTop("-"))
+                    {
+                        stackThrowLessThan2(valueStack.Count);
+                        int value = Calculator(valueStack.Pop(), valueStack.Pop(), operatorStack.Pop());
+                        valueStack.Push(value);
+                    }
+                    operatorStack.Push(t);
+                }
+
+                if (t.Equals("*") || t.Equals("/"))
+                {
+                    operatorStack.Push(t);
+                }
+
+                if (t.Equals("("))
+                {
+                    operatorStack.Push(t);
+                }
+
+                if (t.Equals(")"))
+                {
+                    if (operatorStack.IsOnTop("+") || operatorStack.IsOnTop("-"))
+                    {
+                        stackThrowLessThan2(valueStack.Count);
+                        int v = Calculator(valueStack.Pop(), valueStack.Pop(), operatorStack.Pop());
+                        valueStack.Push(v);
+                    }
+
+                    if (!operatorStack.IsOnTop("("))
+                    {
+                        throw new ArgumentException("No ( found.");
+                    }
+                    operatorStack.Pop();
+
+                    if (operatorStack.IsOnTop("*") || operatorStack.IsOnTop("/"))
+                    {
+                        stackThrowLessThan2(valueStack.Count);
+                        int v = Calculator(valueStack.Pop(), valueStack.Pop(), operatorStack.Pop());
+                        valueStack.Push(v);
+                    }
+                }
+
+            }
+
+            if (operatorStack.Count == 0)
+            {
+                return valueStack.Pop();
+            }
+            else if (operatorStack.Count == 1)
+            {
+                if (operatorStack.IsOnTop("+") || operatorStack.IsOnTop("-"))
+                {
+                    stackThrowLessThan2(valueStack.Count);
+                    return Calculator(valueStack.Pop(), valueStack.Pop(), operatorStack.Pop());
+                }
+
+                throw new ArgumentException("Invalid: operator remains with no values");
+            }
+            else
+            {
+                throw new ArgumentException("Invalid: operator remains with no values");
+            }
+        }
+
+
+        private static int Calculator(int val1, int val2, string op)
+        {
+            switch (op)
+            {
+                case "*":
+                    return val2 * val1;
+                case "/":
+                    if (val1 == 0)
+                    {
+                        throw new ArgumentException("Can not divide by 0");
+                    }
+                    return val2 / val1;
+                case "+":
+                    return val2 + val1;
+                case "-":
+                    return val2 - val1;
+            }
+            return 0;
+        }
+
+        public static bool VariablesVerification(string s)
+        {
+            Regex regex = new Regex(@"^[a-zA-Z]+\d+$");
+          
+
+            return regex.IsMatch(s);
+        }
+
+        private static void stackThrowLessThan2(int e)
+        {
+            if (e < 2)
+            {
+                throw new ArgumentException("Invalid: The value stack contains fewer than 2 values.");
+            }
+        }
+
+        private static void valueStackEmptyCheck(int e)
+        {
+            if (e == 0)
+            {
+                throw new ArgumentException("Invalid: the value stack is empty.");
+            }
+        }
+
+
     }
+
+    static class StackExtentions
+    {
+        public static bool IsOnTop<T>(this Stack<T> stack, string str)
+        { 
+
+
+            if (stack.Count < 1)
+            {
+                return false;
+            }
+            return stack.Peek().Equals(str);
+        }
+    }
+
+
+
+
+
 }
