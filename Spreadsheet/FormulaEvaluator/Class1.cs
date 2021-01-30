a6500a46cfb5a90d5461a4d410294fab8fe74c2f
u1315096@eng.utah.edu <u1315096@eng.utah.edu>
2021-01-29 19:22:49 -0700

    Adding a tests through an application calls "Spreadsheet Solution Tester". Adding comments into the original code.

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -12,16 +12,30 @@ namespace FormulaEvaluator
     public static class Evaluator
     {
 
-
-
+        /*
+         * A method that evaluates integer arithmetic expressions written using standard infix notation.
+         * This method will take in a string input and a delegate from the user. The delegate will have to 
+         * find the correct variables and turn the variables into int numbers. 
+         * 
+         *@param    String input        The string input that contains the integer arithmetic
+         *          Lookup variable     The user own delegate to look up the variables
+         *
+         *@Return   int                 The result number of the arithmetic expressions
+         * 
+         *@throws                       Invalid integer arithmetic
+         * 
+         */
         public static int Evaluate(string input, Lookup variable)
         {
+            //Splits up the string input and put the tokens into an array. 
             string[] substrings = Regex.Split(input, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            //Creates an operator stack and a value stack. 
             Stack<int> valueStack = new Stack<int>();
             Stack<string> operatorStack = new Stack<string>();
             foreach (string t in substrings)
             {
-                //Extension method 
+                //In case of null and empty space, the method will continue to the next token of the substrings array. 
                 if (t.Equals(""))
                 {
                     continue;
@@ -31,65 +45,92 @@ namespace FormulaEvaluator
                     continue;
                 }
 
+                //If token is a number. 
                 if (int.TryParse(t, out int number))
                 {
+                    //If * or / is on top of the operatorStack. Extension IsOnTop was created to simplied the logic of the method.
                     if (operatorStack.IsOnTop("*") || operatorStack.IsOnTop("/"))
                     {
+                        //The private helper method will throws if it is a bad experssion. 
                         valueStackEmptyCheck(valueStack.Count);
+                        /* Performs the calculation between the token and the number that already in the value stack. 
+                         * The calculation is peformed as follow: 
+                         * The value on top of the value stack (operator on top of the operator stack) token.
+                         */
                         int v = Calculator(number, valueStack.Pop(), operatorStack.Pop());
+                        //Pushes the new value into the value stack.
                         valueStack.Push(v);
+                        //Moves to the next token.
                         continue;
                     }
                     else
                     {
+                        //Else, pushes the number into the value stack and moves to the next token.
                         valueStack.Push(number);
                         continue;
                     }
-
-
                 }
 
+                //If token is a + or -
                 if (t.Equals("+") || t.Equals("-"))
                 {
+                    //If + or - is on top of the operatorStack.
                     if (operatorStack.IsOnTop("+") || operatorStack.IsOnTop("-"))
                     {
+                        //The private helper method will throws if it is a bad experssion. 
                         stackThrowLessThan2(valueStack.Count);
+                        /* Performs the calculation between the numbers that already in the . 
+                        * The calculation is peformed as follow:
+                        * The value on top of the value stack (operator on top of the operator stack) The second top value of the value stack.
+                        */
                         int value = Calculator(valueStack.Pop(), valueStack.Pop(), operatorStack.Pop());
+                        //Pushes the new value into the value stack.
                         valueStack.Push(value);
                     }
+                    //Pushes the token into the token stack.
                     operatorStack.Push(t);
                     continue;
                 }
 
+                //If token is a * or /
                 if (t.Equals("*") || t.Equals("/"))
                 {
+                    //Pushes the token into the token stack.
                     operatorStack.Push(t);
                     continue;
                 }
 
+                //If token is a ( 
                 if (t.Equals("("))
                 {
+                    //Pushes the token into the token stack.
                     operatorStack.Push(t);
                     continue;
                 }
 
+
+                //If token is )
                 if (t.Equals(")"))
                 {
+                    //If + or - is on top of the operatorStack. 
                     if (operatorStack.IsOnTop("+") || operatorStack.IsOnTop("-"))
                     {
+                        //Performs the same calculation as above.
                         stackThrowLessThan2(valueStack.Count);
                         int v = Calculator(valueStack.Pop(), valueStack.Pop(), operatorStack.Pop());
                         valueStack.Push(v);
                     }
-
+                    //If there is no ( after the calculation, throws an error. 
                     if (!operatorStack.IsOnTop("("))
                     {
-                        throw new ArgumentException("No ( found.");
+                        throw new ArgumentException("Found ) without proper (");
                     }
                     operatorStack.Pop();
 
+                    //If / or * is on top of the operatorStack. 
                     if (operatorStack.IsOnTop("*") || operatorStack.IsOnTop("/"))
                     {
+                        //Performs the same calculation as above.
                         stackThrowLessThan2(valueStack.Count);
                         int v = Calculator(valueStack.Pop(), valueStack.Pop(), operatorStack.Pop());
                         valueStack.Push(v);
@@ -98,13 +139,17 @@ namespace FormulaEvaluator
                 }
 
 
+                //If token is a variable
                 if (VariablesVerification(t))
                 {
                     if (operatorStack.IsOnTop("*") || operatorStack.IsOnTop("/"))
                     {
+                        //Throws invalid expression.
                         valueStackEmptyCheck(valueStack.Count);
+                        //
                         try
                         {
+                            //Replaces the token with the correct int and calculates the function.
                             int v = Calculator(variable(t), valueStack.Pop(), operatorStack.Pop());
                             valueStack.Push(v);
                             continue;
@@ -112,23 +157,23 @@ namespace FormulaEvaluator
 
                         catch
                         {
+                            //If the variable does not exist, throws 
                             throw new ArgumentException("Can not find variable.");
                         }
 
                     }
                     else
                     {
+                        //Else pushes the variable onto the value stack.
                         valueStack.Push(variable(t));
                     }
                 }
-
-
-
-
             }
 
+            //If the condition is met and the expression is valid, returns the final value.
             if (operatorStack.Count == 0)
             {
+                valueStackEmptyCheck(valueStack.Count);
                 return valueStack.Pop();
             }
             else if (operatorStack.Count == 1)
@@ -148,6 +193,18 @@ namespace FormulaEvaluator
         }
 
 
+        /*
+         * A private helper method to perform simple calculation.
+         * 
+         * @param   int val1    The first value
+         *          int va2     The second value
+         *          string op   The operator
+         *          
+         *@Return   int value   The new value after calculation.
+         * 
+         *@throws   if there is a division by 0.
+         * 
+         */
         private static int Calculator(int val1, int val2, string op)
         {
             switch (op)
@@ -168,8 +225,21 @@ namespace FormulaEvaluator
             return 0;
         }
 
-        public static bool VariablesVerification(string v)
+        /*
+      * A private helper to help identify the pattern of the variable.
+      * variables consisting of one or more letters followed by one or more digits. 
+      * Letters can be lowercase or uppercase.
+      * 
+      * @param  string v    variable we needs to identify.
+      * 
+      * @Return bool        If the variable is valid, returns true. Otherwise returns false.   
+      * 
+      * @throws             If the variable is invalid.
+      * 
+      */
+        private static bool VariablesVerification(string v)
         {
+
             Regex regex = new Regex(@"\b[a-zA-Z]+\d+\b");
             bool boo = regex.IsMatch(v);
             if (boo == false)
@@ -180,19 +250,29 @@ namespace FormulaEvaluator
             return regex.IsMatch(v);
         }
 
+        /*
+         *A private method to throw if there is invalid expression.
+         *
+         *@throws             If the expression is invalid.
+         */
         private static void stackThrowLessThan2(int e)
         {
             if (e < 2)
             {
-                throw new ArgumentException("Invalid: The value stack contains fewer than 2 values.");
+                throw new ArgumentException("Invalid expression.");
             }
         }
 
+        /*
+        *A private method to throw if there is invalid expression.
+        *
+        *@throws             If the expression is invalid.
+        */
         private static void valueStackEmptyCheck(int e)
         {
             if (e == 0)
             {
-                throw new ArgumentException("Invalid: the value stack is empty.");
+                throw new ArgumentException("Invalid expression.");
             }
         }
 
@@ -201,10 +281,15 @@ namespace FormulaEvaluator
 
     static class StackExtentions
     {
+
+        /*
+         * This method will check the string on top of the stack.
+         * 
+         * @param   String str  The string on top of the stack. 
+         * @Return  bool        Returns true if the string str is on top of the stack. Otherwise returns false
+         */
         public static bool IsOnTop<T>(this Stack<T> stack, string str)
         {
-
-
             if (stack.Count < 1)
             {
                 return false;
@@ -212,9 +297,5 @@ namespace FormulaEvaluator
             return stack.Peek().Equals(str);
         }
     }
-
-
-
-
 
 }
---
+++ Spreadsheet/Spreadsheet Solution Tester/Program.cs
@@ -0,0 +1,146 @@
+﻿using FormulaEvaluator;
+using System;
+
+namespace Spreadsheet_Solution_Tester
+{
+
+    class Program
+    {
+
+
+        static int SimpleLookup(string s)
+        {
+            return 7;
+        }
+
+        static int AdvanceLookup(string s)
+        {
+            if (s == "a1")
+            {
+                return 1;
+            }
+
+            if (s == "A7")
+            {
+                return 2;
+            }
+
+            if (s == "aa1")
+            {
+                return 3;
+            }
+            else throw new ArgumentException("unknow variable");
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+        
+            //Normal test, if the test is correct, the statement will print true.
+            Console.WriteLine("If the test is correct, the statement will print True.");
+            Console.WriteLine("Test 1:");
+            Console.WriteLine((Evaluator.Evaluate("1+a111", SimpleLookup)) == 8);
+            Console.WriteLine("Test 2:");
+            Console.WriteLine((Evaluator.Evaluate("1+aaa111", SimpleLookup)) == 8);
+            Console.WriteLine("Test 3:");
+            Console.WriteLine((Evaluator.Evaluate("1+aaa11111", SimpleLookup)) == 8);
+            Console.WriteLine("Test 4:");
+            Console.WriteLine((Evaluator.Evaluate("1+aaaa1", SimpleLookup)) == 8);
+            Console.WriteLine("Test 5:");
+            Console.WriteLine((Evaluator.Evaluate("  1   +  2  + a111 ", SimpleLookup)) == 10);
+            Console.WriteLine("Test 6:");
+            Console.WriteLine((Evaluator.Evaluate("(1+2)*A7", AdvanceLookup)) == 6);
+            Console.WriteLine("Test 7:");
+            Console.WriteLine((Evaluator.Evaluate("aa1/7", AdvanceLookup)) == 0);
+            Console.WriteLine("Test 8:");
+            Console.WriteLine((Evaluator.Evaluate("((1+2))", SimpleLookup)) == 3);
+            Console.WriteLine("Test 9:");
+            Console.WriteLine("Test 10:");
+            Console.WriteLine((Evaluator.Evaluate("1+2*5", SimpleLookup)) == 11);
+            Console.WriteLine("Test 11:");
+            Console.WriteLine((Evaluator.Evaluate("(((1+2)*5)+3)/2", SimpleLookup)) == 9);
+
+
+            Console.WriteLine("");
+            Console.WriteLine("If the test is correct, the statement will print Passed.");
+            Console.WriteLine("Test 1:");
+            try
+            {
+                Console.WriteLine(Evaluator.Evaluate("++", SimpleLookup));
+                Console.WriteLine("Failed");
+            }
+            catch
+            {
+                Console.WriteLine("Passed");
+            }
+            Console.WriteLine("Test 2:");
+            try
+            {
+                Console.WriteLine(Evaluator.Evaluate("(1+2", SimpleLookup));
+                Console.WriteLine("Failed");
+            }
+            catch
+            {
+                Console.WriteLine("Passed");
+            }
+            Console.WriteLine("Test 3:");
+            try
+            {
+                Console.WriteLine(Evaluator.Evaluate("5/0", SimpleLookup));
+                Console.WriteLine("Failed");
+            }
+            catch
+            {
+                Console.WriteLine("Passed");
+            }
+            Console.WriteLine("Test 4:");
+            try
+            {
+                Console.WriteLine(Evaluator.Evaluate("1+2s", SimpleLookup));
+                Console.WriteLine("Failed");
+            }
+            catch
+            {
+                Console.WriteLine("Passed");
+            }
+            Console.WriteLine("Test 5:");
+            try
+            {
+                Console.WriteLine(Evaluator.Evaluate("1+2)", SimpleLookup));
+                Console.WriteLine("Failed");
+            }
+            catch
+            {
+                Console.WriteLine("Passed");
+            }
+            Console.WriteLine("Test 6:");
+            try
+            {
+                Console.WriteLine(Evaluator.Evaluate("(((())))", SimpleLookup));
+                Console.WriteLine("Failed");
+            }
+            catch
+            {
+                Console.WriteLine("Passed");
+            }
+            Console.WriteLine("Test 7:");
+            try
+            {
+                Console.WriteLine(Evaluator.Evaluate("a111+3", AdvanceLookup));
+                Console.WriteLine("Failed");
+            }
+            catch
+            {
+                Console.WriteLine("Passed");
+            }
+
+            Console.Read();
+
+
+
+
+        }
+    }
+}
