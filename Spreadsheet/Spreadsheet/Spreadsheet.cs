ed0525a97fa755a1adf15cdadcb0c9ae18006950
u1315096@eng.utah.edu <u1315096@eng.utah.edu>
2021-02-17 19:46:38 -0700

    Change some logic to ultilizing getcelsstorecalculate in abstract class

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -10,44 +10,66 @@ namespace SS
     public class Spreadsheet : AbstractSpreadsheet
     {
         private Dictionary<string, Cell> spreadsheet;
-
+        private DependencyGraph graph;
         public override object GetCellContents(string name)
         {
-
             if (!nameValidation(name))
             {
                 throw new InvalidNameException();
             }
 
+            if (spreadsheet[name].GetType() == typeof(Formula))
+            {
 
+                return (Formula)spreadsheet[name].getContent();
+            }
 
-            throw new NotImplementedException();
+            if (spreadsheet[name].GetType() == typeof(double))
+            {
+
+                return (double)spreadsheet[name].getContent();
+            }
+
+            return (string)spreadsheet[name].getContent();
+            // throw new NotImplementedException();
         }
 
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
 
-            List<string> nonEmptyCells = new List<string>();
-            foreach (KeyValuePair<string, Cell> entry in spreadsheet)
-            {
-                if (!spreadsheet[entry.Key].Equals(""))
-                {
-                    nonEmptyCells.Add(entry.Key);
-                }
-            }
-            return new List<string>(nonEmptyCells);
-   
+            //List<string> nonEmptyCells = new List<string>();
+
+            //foreach (KeyValuePair<string, Cell> entry in spreadsheet)
+            //{
+            //    //if (!spreadsheet[entry.Key].Equals(""))
+            //    //{
+            //        nonEmptyCells.Add(entry.Key);
+            //   // }
+            //}
+            //Remove every empty cell 
+            return new List<string>(spreadsheet.Keys);
         }
 
         public override IList<string> SetCellContents(string name, double number)
         {
-
             if (!nameValidation(name))
             {
                 throw new InvalidNameException();
             }
+           
+            spreadsheet[name].setContent(number);
 
-            throw new NotImplementedException();
+            return new List<string>(GetCellsToRecalculate(name));
+
+            //HashSet<string> list = new HashSet<string>();
+            //list.Add(name);
+            //list.Union(graph.GetDependents(name));
+            //foreach(string e in graph.GetDependents(name))
+            //{
+            //    list.Union(graph.GetDependents(e));
+            //}
+            //return new List<string>(list);
+            // throw new NotImplementedException();
         }
 
         public override IList<string> SetCellContents(string name, string text)
@@ -59,7 +81,36 @@ namespace SS
                 throw new InvalidNameException();
             }
 
-            throw new NotImplementedException();
+            //if (text.Equals(""))
+            //{
+            //    spreadsheet[name].setContent(text);
+            //    spreadsheet.Remove(name);
+            //    HashSet<string> list2 = new HashSet<string>();
+
+            //    list2.Add(name);
+
+            //    list2.Union(graph.GetDependents(name));
+
+            //    foreach (string e in graph.GetDependents(name))
+            //    {
+            //        list2.Union(graph.GetDependents(e));
+            //    }
+            //    return new List<string>(list2);
+            //}
+
+            spreadsheet[name].setContent(text);
+
+            return new List<string>(GetCellsToRecalculate(name));
+
+            //HashSet<string> list = new HashSet<string>();
+            //list.Add(name);
+            //list.Union(graph.GetDependents(name));
+            //foreach (string e in graph.GetDependents(name))
+            //{
+            //    list.Union(graph.GetDependents(e));
+            //}
+            //return new List<string>(list);
+            // throw new NotImplementedException();
         }
 
         public override IList<string> SetCellContents(string name, Formula formula)
@@ -69,7 +120,24 @@ namespace SS
             {
                 throw new InvalidNameException();
             }
-            throw new NotImplementedException();
+
+            spreadsheet[name].setContent(formula);
+            return new List<string>(GetCellsToRecalculate(name));
+
+            //if (formula.ToString().Contains(name))
+            //{
+            //    graph.AddDependency(name, formula.ToString);
+            //}
+            //spreadsheet[name].setContent(formula);
+            //HashSet<string> list = new HashSet<string>();
+            //list.Add(name);
+            //list.Union(GetDirectDependents(name));
+            //foreach (string e in graph.GetDependents(name))
+            //{
+            //    list.Union(GetDirectDependents(e));
+            //}
+            //return new List<string>(list);
+            // throw new NotImplementedException();
         }
 
         protected override IEnumerable<string> GetDirectDependents(string name)
@@ -79,7 +147,8 @@ namespace SS
             {
                 throw new InvalidNameException();
             }
-            throw new NotImplementedException();
+            return graph.GetDependents(name);
+          //  throw new NotImplementedException();
         }
 
 
@@ -100,49 +169,34 @@ namespace SS
     class Cell
     {
 
-      
-        public Object content;
-        public double value;
 
-        public Cell(Object _content, double _value)
+        public Object content;
+        // public double value;
+
+        public Cell()
+        {
+            content = null;
+        }
+        public Cell(Object _content)
         {
             content = _content;
-            value = _value;
+            //   value = _value;
         }
 
-        public object getContent(string name)
+        public object getContent()
         {
-            if (content is null )
+            if (content is null)
             {
                 return "";
             }
-
-    
             return content;
         }
 
-
+        public void setContent(object obj)
+        {
+            content = obj;
+        }
 
     }
 
-    //public class Student
-    //{
-    //    private string major;
-    //    public int ID;
-    //    public float GPA;
-
-
-    //    public Student(string _major, int _ID, float _GPA)
-    //    {
-    //        ID = _ID;
-    //        major = _major;
-    //        GPA = _GPA;
-    //    }
-
-    //    public string GetMajor()
-    //    {
-    //        return major;
-    //    }
-
-    //}
 }
